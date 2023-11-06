eb5186ad42e341450946d07652c12e1a0edaf165
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-06 16:22:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "my_security_group" {
 
 # Launch an EC2 instance with provisioner "remote-exec"
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-05c13eab67c5d8861"
+  ami           = ""
   instance_type = "t2.micro"
-  key_name      = "hello" # Replace with your SSH key pair
+  key_name      = "" # Replace with your SSH key pair
   security_groups = [aws_security_group.my_security_group.name]
 
   user_data = <<-EOF
