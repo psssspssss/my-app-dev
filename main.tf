d537e1fb892e26ca2175d721ce51fb3d4edcbcff
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-06 15:03:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "my_vpc" {
-  id = "vpc-08d04c9a4a6db7601"
-}
-
-# Create a subnet within the VPC
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1b"
-}
-
 # Create a security group for the EC2 instance
 resource "aws_security_group" "my_security_group" {
   name_prefix = "my-ec2-sg"
@@ -38,7 +26,6 @@ resource "aws_security_group" "my_security_group" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-05c13eab67c5d8861"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnet.id
   key_name      = "hello" # Replace with your SSH key pair
   security_groups = [aws_security_group.my_security_group.name]
 
@@ -53,7 +40,6 @@ resource "aws_instance" "my_ec2" {
               service httpd restart
               EOF
 }
-
 
 output "public_ip" {
   value = aws_instance.my_ec2.public_ip
