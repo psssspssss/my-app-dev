341e36a06969920068fbcf293acf29635c688c16
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-06 07:31:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,106 +1,12 @@
-# Define the AWS provider configuration.
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region.
+  region = "us-east-1" # Specify your desired AWS region
 }
 
-variable "cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "key_name" {
-  description = "Name of the AWS key pair"
-  type        = string
-  default     = "Dev" # Replace with your default key name or provide it during runtime
-}
-
-resource "aws_key_pair" "Dev" {
-  key_name   = var.key_name
-  public_key = file("C:\Users\vinod/.ssh/id_rsa") # Replace with the path to your public key file
-}
-
-
-resource "aws_vpc" "myvpc" {
-  cidr_block = var.cidr
-  tags = {
-    Name = "test_dev_vpc"
-  }
-}
-
-resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "test_dev_subnet"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myvpc.id
-  tags = {
-    Name = "test_dev_igw"
-  }
-}
-
-resource "aws_route_table" "RT" {
-  vpc_id = aws_vpc.myvpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-0fc5d935ebf8bc3bc" # Specify the AMI ID of the EC2 instance
+  instance_type = "t2.micro"             # Specify the instance type
 
   tags = {
-    Name = "test_dev_route_table"
-  }
-}
-
-resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
-  route_table_id = aws_route_table.RT.id
-}
-
-
-resource "aws_security_group" "webSg" {
-  name   = "web"
-  vpc_id = aws_vpc.myvpc.id
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "test_dev_web_sg"
-  }
-}
-
-resource "aws_instance" "server" {
-  ami                    = "ami-05c13eab67c5d8861"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.Dev.key_name
-  vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub1.id
-  tags = {
-    Name = "test_dev_instance"
+    Name = "example-instance"
   }
 }
