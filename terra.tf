1edeec671dcc6aecc0c067a1a5fbb743fd1b97f8
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-06 12:55:54 +0530

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,11 +1,32 @@
 provider "aws" {
-  region = "us-east-1" # Specify your desired AWS region
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "example-subnet"
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fc5d935ebf8bc3bc" # Specify the AMI ID of the EC2 instance
-  instance_type = "t2.micro"             # Specify the instance type
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+  key_name      = "hello" # Replace with your existing key name
 
+  subnet_id = aws_subnet.example_subnet.id
   tags = {
     Name = "example-instance"
   }
