c85899c5bf90dc08f20fd5416a968a8b3aab4a6d
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-09 18:25:33 +0530

    done

--- main.tf
+++ main.tf
@@ -31,9 +31,9 @@ resource "aws_internet_gateway" "my_internet_gateway" {
 
 # Launch an EC2 instance with provisioner "remote-exec"
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-05c13eab67c5d8861"  # Replace with your desired AMI ID
+  ami           = "ami-0fc5d935ebf8bc3bc"  # Replace with your desired AMI ID
   instance_type = "t2.micro"
-  key_name      = "hello"  # Replace with your SSH key pair
+  key_name      = "DEv"  # Replace with your SSH key pair
   security_groups = [aws_security_group.my_security_group.name]
 
   connection {
