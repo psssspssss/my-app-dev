28da1b88967dc2195981b11481b98801a10455f1
vandana-dsu <141351192+vandana-dsu@users.noreply.github.com>
2023-11-23 20:28:40 +0530

    Delete myapp directory

--- myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.name
